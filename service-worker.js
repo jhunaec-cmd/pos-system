24d88d0502d46aa0068d961fdbf2120a4cdb4dce
Halim <jhunaec@gmail.com>
2026-07-15 22:57:39 +0800

    Redesign app icon as a professional payment-terminal glyph

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
   file, so returning users get the new version instead of a stale cache.
 */
 
-const CACHE_NAME = "pos-cache-v11";
+const CACHE_NAME = "pos-cache-v12";
 
 const APP_SHELL_FILES = [
   "./",
