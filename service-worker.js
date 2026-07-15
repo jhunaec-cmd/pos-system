1f34c10e06fe8efb1bc11fae64c91a240cae27ab
Halim <jhunaec@gmail.com>
2026-07-15 20:13:00 +0800

    Fix device code and barcode fields auto-capitalizing on mobile keyboards

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
   file, so returning users get the new version instead of a stale cache.
 */
 
-const CACHE_NAME = "pos-cache-v7";
+const CACHE_NAME = "pos-cache-v8";
 
 const APP_SHELL_FILES = [
   "./",
